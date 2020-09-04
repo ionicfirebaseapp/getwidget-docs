ce4989eb1d71149af4565b7ba04faaf6581d9c5e
Navin Sharma <pietechSolution@gmail.com>
2020-09-04 11:39:53 +0530

    Update config.js

--- src/.vuepress/config.js
+++ src/.vuepress/config.js
@@ -8,7 +8,7 @@ module.exports = {
     /**
      * Ref：https://v1.vuepress.vuejs.org/config/#description
      */
-    description: GetwidgetDocument ,
+    description: description ,
 
     /**
      * Extra tags to be injected to the page HTML `<head>`
